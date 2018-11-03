92b0c84588c66cd238381f5f56b3aad6eb41d370
Charles A. McCown <charlesmccown@gmail.com>
2018-11-03 11:41:51 -0500

    update two resolutions

--- config/hyperv720.sh
+++ config/hyperv720.sh
@@ -2,7 +2,7 @@ sudo echo """GRUB_DEFAULT=0
 GRUB_TIMEOUT_STYLE=hidden
 GRUB_TIMEOUT=10
 GRUB_DISTRIBUTOR=\`lsb_release -i -s 2> /dev/null || echo Debian\`
-GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash video=hyperv_fb:1920x1080\"
+GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash video=hyperv_fb:1280x720\"
 GRUB_CMDLINE_LINUX=\"\"
 """ > /etc/default/grub
 
